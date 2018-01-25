64408a2f7ef6d2adbc1d11919501a38d11ec2718
Michael Meisel <mrmeisel@gmail.com>
2018-01-25 10:03:56 -0800

    Move site logic to a service

--- routes/sites.js
+++ routes/sites.js
@@ -1,116 +1,32 @@
 const router = require('express').Router()
 const sequelize = require('../db')
-const Site = require('../models/site')
+const siteService = require('../services/site-service')
 
 router.get('/', (req, res) => {
-  let where = { userId: req.user.id }
-  let attributes
+  // By default, only return the domains and their revs. Only if the "settings" request parameter is
+  // present (with any value), return all of the settings details, too.
+  let includeSettings = req.query.hasOwnProperty('settings')
+  let domains = null
 
   // If there is a list of domains in the query string, return only those domains. The query
   // parameter "d" is used to reduce the chance of reaching the URL character limit
   if (req.query.hasOwnProperty('d')) {
-    where.domain = Array.isArray(req.query.d) ? req.query.d : [req.query.d]
+    domains = Array.isArray(req.query.d) ? req.query.d : [req.query.d]
   }
 
-  // By default, only return the domains and their revs. Only if the "settings" request parameter is
-  // present (with any value), return all of the settings details, too.
-  if (req.query.hasOwnProperty('settings')) {
-    attributes = { exclude: ['id', 'userId', 'createdAt', 'updatedAt'] }
-  } else {
-    attributes = ['domain', 'rev']
-  }
-
-  Site.findAll({ where, attributes })
+  siteService.get(req.user.id, domains, includeSettings)
     .then(sites => res.json(sites))
 })
 
 // This is the main sync endpoint. It should be called with all domains that need to be synced, as
 // determined by a call to the GET endpoint.
 router.patch('/', (req, res) => {
-  const remoteSiteMap = req.body
-
-  // TODO: validate remoteSiteMap format
-
+  // TODO: validate body format (either here or in the service)
   sequelize.transaction(transaction => {
-    Site.findAll({
-      where: { userId: req.user.id, domain: Object.keys(remoteSiteMap) },
-      attributes: { exclude: ['createdAt', 'updatedAt'] },
-      transaction
-    })
-    .then(localSites => {
-      const localOnly = {}
-      const remoteOnly = {}
-      const matches = {}
-
-      // Note that remoteSiteMap is an object mapping domain names to settings, but localSites is
-      // an array with the domain in the object
-
-      // Find out which domains are in both places versus not present remotely (represented by the
-      // domain mapping to null in the input)
-      localSites.forEach(localSite => {
-        const remoteSite = remoteSiteMap[localSite.domain]
-
-        if (remoteSite == null) {
-          localOnly[localSite.domain] = localSite
-        } else {
-          matches[localSite.domain] = { local: localSite, remote: remoteSite }
-        }
-      })
-
-      // Look for remote sites that we don't have locally
-      Object.keys(remoteSiteMap).forEach(remoteDomain => {
-        if (!localOnly.hasOwnProperty(remoteDomain) && !matches.hasOwnProperty(remoteDomain)) {
-          remoteOnly[remoteDomain] = remoteSiteMap[remoteDomain]
-        }
-      })
-
-      // Figure out which sites from matchedDomains need to be updates
-      const remoteUpdateMap = {}
-      // Add remoteOnlyDomains to localUpdates so they'll be inserted
-      const localUpdates = remoteOnly.map(domain => remoteToLocal(domain, remoteSiteMap[domain]))
-
-      Object.keys(matches).forEach(domain => {
-        const match = matches[domain]
-
-        if (match.local.rev !== match.remote.rev) {
-          // Check for ancestry
-          if (match.local.history.includes(match.remote.rev)) {
-            // We have a newer version locally, send this to the remote
-            remoteUpdateMap[domain] = localToRemote(match.local)
-          } else if (match.remote.history.includes(match.local.rev)) {
-            // They have a newer version remotely, replace the local version
-            localUpdates.push(remoteToLocal(domain, match.remote))
-          } else {
-            // TODO: CONFLICT! Just log something for now.
-            console.warn('Conflict for domain', domain, match.local, match.remote)
-          }
-        }
-      })
-
-      res.json(remoteUpdateMap)
-
-      // TODO: do I need updateOnDuplicate here, or will it update everything that's specified?
-      return Site.bulkCreate(localUpdates, { transaction })
-    })
+    siteService.sync(req.user.id, req.body, { transaction })
   })
-
-  function localToRemote (localSettings) {
-    return Object.assign({}, localSettings, { id: undefined, userId: undefined, domain: undefined })
-  }
-
-  function remoteToLocal (domain, remoteSettings) {
-    return Object.assign({}, remoteSettings, { id: undefined, userId: req.user.id, domain })
-  }
-})
-
-router.get('/test', (req, res) => {
-  res.json(new Site({
-    userId: req.user.id,
-    domain: 'test.com',
-    generation: 1,
-    pwLength: 16,
-    symbols: true
-  }))
+  .then(result => res.json(result))
+  .catch(error => res.json({ error }))
 })
 
 module.exports = router
---
+++ services/site-service.js
@@ -0,0 +1,97 @@
+const Site = require('../models/site')
+
+const siteService = {}
+
+siteService.get = (userId, options) => {
+  let where = { userId }
+  let attributes
+
+  if (options.domains) {
+    where.domain = options.domains
+  }
+
+  if (options.includeSettings) {
+    attributes = { exclude: ['id', 'userId', 'createdAt', 'updatedAt'] }
+  } else {
+    attributes = ['domain', 'rev']
+  }
+
+  return Site.findAll({ where, attributes, transaction: options.transaction })
+}
+
+siteService.sync = (userId, remoteSiteMap, options) => {
+  return Site.findAll({
+    where: { userId, domain: Object.keys(remoteSiteMap) },
+    attributes: { exclude: ['createdAt', 'updatedAt'] },
+    transaction: options.transaction
+  })
+  .then(localSites => {
+    const localOnly = {}
+    const remoteOnly = {}
+    const matches = {}
+
+    // Note that remoteSiteMap is an object mapping domain names to settings, but localSites is
+    // an array with the domain in the object
+
+    // Find out which domains are in both places versus not present remotely (represented by the
+    // domain mapping to null in the input)
+    localSites.forEach(localSite => {
+      const remoteSite = remoteSiteMap[localSite.domain]
+
+      if (remoteSite == null) {
+        localOnly[localSite.domain] = localSite
+      } else {
+        matches[localSite.domain] = { local: localSite, remote: remoteSite }
+      }
+    })
+
+    // Look for remote sites that we don't have locally
+    Object.keys(remoteSiteMap).forEach(remoteDomain => {
+      if (!localOnly.hasOwnProperty(remoteDomain) && !matches.hasOwnProperty(remoteDomain)) {
+        remoteOnly[remoteDomain] = remoteSiteMap[remoteDomain]
+      }
+    })
+
+    // Figure out which sites from matchedDomains need to be updates
+    const remoteUpdateMap = {}
+    // Add remoteOnlyDomains to localUpdates so they'll be inserted
+    const localUpdates = remoteOnly.map(domain => remoteToLocal(domain, remoteSiteMap[domain]))
+    const conflictMap = {}
+
+    Object.keys(matches).forEach(domain => {
+      const match = matches[domain]
+
+      if (match.local.rev !== match.remote.rev) {
+        // Check for ancestry
+        if (match.local.history.includes(match.remote.rev)) {
+          // We have a newer version locally, send this to the remote
+          remoteUpdateMap[domain] = localToRemote(match.local)
+        } else if (match.remote.history.includes(match.local.rev)) {
+          // They have a newer version remotely, replace the local version
+          localUpdates.push(remoteToLocal(domain, match.remote))
+        } else {
+          // There's a conflict, put our version in the "rejected" list
+          conflictMap[domain] = localToRemote(match.local)
+        }
+      }
+    })
+
+    // TODO: do I need updateOnDuplicate here, or will it update everything by default?
+    return Site.bulkCreate(localUpdates, { transaction: options.transaction })
+      .then(() => ({
+        accepted: localUpdates.map(site => site.domain),
+        changed: remoteUpdateMap,
+        rejected: conflictMap
+      }))
+  })
+
+  function localToRemote (localSettings) {
+    return Object.assign({}, localSettings, { id: undefined, userId: undefined, domain: undefined })
+  }
+
+  function remoteToLocal (domain, remoteSettings) {
+    return Object.assign({}, remoteSettings, { id: undefined, userId, domain })
+  }
+}
+
+module.exports = siteService
