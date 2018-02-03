810d3ae534a7c58b7dd38791debd1727687ec7db
Michael Meisel <mrmeisel@gmail.com>
2018-02-03 10:45:11 -0800

    Remove unused views and related dependencies

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express()
 const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const bodyParser = require('body-parser')
-const expressReactViews = require('express-react-views')
 
 const authInit = require('./auth/init')
 const authRoutes = require('./auth/routes')
@@ -37,11 +36,6 @@ authInit(app)
 
 // Other middleware
 app.use(bodyParser.json())
-
-// Views
-app.set('views', './views')
-app.set('view engine', 'jsx')
-app.engine('jsx', expressReactViews.createEngine())
 
 // Routes
 app.use('/auth', authRoutes)
